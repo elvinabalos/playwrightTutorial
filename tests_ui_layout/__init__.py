45990179a2b10ef18e81c404852c9ba1a2a50ab7
macbook pro <macbookpro@macbooks-macbook-pro.local>
2025-02-26 16:26:51 +0800

    moved password to secret_config.py and edit file & rename test_parameterize.py and

--- tests_ui_layout/__init__.py
+++
@@ -1,8 +0,0 @@
-class ShopWomen:
-    pass
-
-
-#packages/folder : "test*"; Example: "tests_ui_layout"
-# python files: "test*"; Example: "test_home_page_layout"
-# python functions/modules: "test*" Example: "test_us_section_verbiage"
-# python classes: "Test*" Example "TestHomePage"
